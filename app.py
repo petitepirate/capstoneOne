2d79641cc3d86dcad43f6b43e28ad9ce248139a3
root <root@DESKTOP-NIR74OK.localdomain>
2020-11-29 21:07:27 -0800

    basic routes created

---
+++ app.py
@@ -0,0 +1,242 @@
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///psopayscale2'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "shenanigans"
+
+connect_db(app)
+db.create_all()
+# toolbar = DebugToolbarExtension(app)
+
+
+#### HOME ROUTES ####
+@app.route("/")
+def enterpage():
+    data = open('index.html').read()    
+    return data
+
+@app.route("/home")
+def homepage():
+    return render_template('index2.html')
+
+@app.route("/about")
+def aboutpage():
+    return render_template('about.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Custom 404 Page"""
+
+    return render_template('404.html'), 404
+# @app.route("/regions")
+# def aboutpage():
+#     return render_template('regions.html')
+
+
+#### USERS ROUTES #####
+@app.route("/user/new", methods=["GET"])
+def users_new_form():
+    """Show a form to create a new user"""
+
+    return render_template('new_user.html')
+
+@app.route("/user/new", methods=["POST"])
+def add_user():
+    new_user = User(
+        user_name=request.form["user_name"],
+        first_name=request.form["first_name"],
+        last_name=request.form["last_name"],
+        email=request.form["email"],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/home')
+    # return redirect('/user/info/<int:user_id>')
+
+
+@app.route("/user/info/<int:user_id>", methods=["GET"])
+def user_page(user_id):
+    user = User.query.get_or_404(user_id)
+    image_url = user.image_url
+    return render_template('user_info.html',  user=user, image_url=image_url)
+
+@app.route("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    """Show edit form"""
+    user = User.query.get_or_404(user_id)
+    return render_template("edit_user.html", user=user)
+
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def submit_edit(user_id):
+    """Edit a user"""
+
+    user = User.query.get_or_404(user_id)
+    user_name=request.form["user_name"]
+    first_name=request.form["first_name"]
+    last_name=request.form["last_name"]
+    email=request.form["email"]
+    image_url=request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/user/info/<int:user_id>")
+
+
+#### JOBS ROUTES #####
+
+
+
+
+
+
+
+
+
+#### AUTHENTICATION #####
+
+
+
+
+
+
+
+
+#### INITIAL ROUTES ####
+@app.route("/ocean/01", methods=["GET"])
+def ocean1_page():
+    return render_template('/initial_routes/square01.html')
+    
+@app.route("/ocean/02", methods=["GET"])
+def ocean2_page():
+    return render_template('/initial_routes/square02.html')
+
+@app.route("/ocean/03", methods=["GET"])
+def ocean3_page():
+    return render_template('/initial_routes/square03.html')
+
+@app.route("/ocean/04", methods=["GET"])
+def ocean4_page():
+    return render_template('/initial_routes/square04.html')
+
+@app.route("/ocean/05", methods=["GET"])
+def ocean5_page():
+    return render_template('/initial_routes/square05.html')
+
+@app.route("/ocean/06", methods=["GET"])
+def ocean6_page():
+    return render_template('/initial_routes/square06.html')
+
+@app.route("/ocean/07", methods=["GET"])
+def ocean7_page():
+    return render_template('/initial_routes/square07.html')
+
+@app.route("/ocean/08", methods=["GET"])
+def ocean8_page():
+    return render_template('/initial_routes/square08.html')
+
+@app.route("/ocean/09", methods=["GET"])
+def ocean9_page():
+    return render_template('/initial_routes/square09.html')
+
+@app.route("/ocean/10", methods=["GET"])
+def ocean10_page():
+    return render_template('/initial_routes/square10.html')
+
+@app.route("/ocean/11", methods=["GET"])
+def ocean11_page():
+    return render_template('/initial_routes/square11.html')
+
+@app.route("/ocean/12", methods=["GET"])
+def ocean12_page():
+    return render_template('/initial_routes/square12.html')
+
+@app.route("/ocean/13", methods=["GET"])
+def ocean13_page():
+    return render_template('/initial_routes/square13.html')
+
+@app.route("/ocean/14", methods=["GET"])
+def ocean14_page():
+    return render_template('/initial_routes/square14.html')
+
+@app.route("/ocean/15", methods=["GET"])
+def ocean15_page():
+    return render_template('/initial_routes/square15.html')
+
+@app.route("/ocean/16", methods=["GET"])
+def ocean16_page():
+    return render_template('/initial_routes/square16.html')
+    
+@app.route("/ocean/17", methods=["GET"])
+def ocean17_page():
+    return render_template('/initial_routes/square17.html')
+
+@app.route("/ocean/18", methods=["GET"])
+def ocean18_page():
+    return render_template('/initial_routes/square18.html')
+
+@app.route("/ocean/19", methods=["GET"])
+def ocean19_page():
+    return render_template('/initial_routes/square19.html')
+
+@app.route("/ocean/20", methods=["GET"])
+def ocean20_page():
+    return render_template('/initial_routes/square20.html')
+
+@app.route("/ocean/21", methods=["GET"])
+def ocean21_page():
+    return render_template('/initial_routes/square21.html')
+
+@app.route("/ocean/22", methods=["GET"])
+def ocean22_page():
+    return render_template('/initial_routes/square22.html')
+
+@app.route("/ocean/23", methods=["GET"])
+def ocean23_page():
+    return render_template('/initial_routes/square23.html')
+
+@app.route("/ocean/24", methods=["GET"])
+def ocean24_page():
+    return render_template('/initial_routes/square24.html')
+
+@app.route("/ocean/25", methods=["GET"])
+def ocean25_page():
+    return render_template('/initial_routes/square25.html')
+
+@app.route("/ocean/26", methods=["GET"])
+def ocean26_page():
+    return render_template('/initial_routes/square26.html')
+
+@app.route("/ocean/27", methods=["GET"])
+def ocean27_page():
+    return render_template('/initial_routes/square27.html')
+
+@app.route("/ocean/28", methods=["GET"])
+def ocean28_page():
+    return render_template('/initial_routes/square28.html')
+
+@app.route("/ocean/29", methods=["GET"])
+def ocean29_page():
+    return render_template('/initial_routes/square29.html')
+
+@app.route("/ocean/30", methods=["GET"])
+def ocean30_page():
+    return render_template('/initial_routes/square30.html')
+
+@app.route("/ocean/31", methods=["GET"])
+def ocean31_page():
+    return render_template('/initial_routes/square31.html')
+
+@app.route("/ocean/32", methods=["GET"])
+def ocean32_page():
+    return render_template('/initial_routes/square32.html')
+
+
+#### INDIVIDUAL COUNTRIES ROUTES ####
---
+++ models.py
@@ -0,0 +1,39 @@
+from flask_sqlalchemy import SQLAlchemy
+#from flask_bcrpyt import Bcrypt
+
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+DEFAULT_IMG = "https://images.unsplash.com/photo-1562037283-072818fb6d8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=250&q=80"
+
+class User(db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.Text, nullable=False, unique=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    image_url = db.Column(db.Text,
+                          nullable=False, default=DEFAULT_IMG)
+    password = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        u = self
+        return f"<User id={u.id} user_name={u.user_name} first_name={u.first_name} last_name={u.last_name} email={u.email}>"
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    job_title = db.Column(db.Text, nullable=False, unique=True)
+    location = db.Column(db.Text, nullable=False)
+    start_year = db.Column(db.Integer, nullable=False)
+    day_rate = db.Column(db.Integer, nullable=False, unique=True)
+    user_name = db.Column(db.Text, db.ForeignKey('users.user_name'))
