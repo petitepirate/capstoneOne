9e3c1b7764efc55ef3b3ca01199f771217a8bb7c
megan <msmeganmcmanus@gmail.com>
2020-12-01 15:10:35 -0800

    update seed file and model

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_name = db.Column(db.Text, nullable=False, unique=True)
+    user_name = db.Column(db.String(20), primary_key=True, nullable=False, unique=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     email = db.Column(db.Text, nullable=False, unique=True)
@@ -31,9 +31,13 @@ class User(db.Model):
         return f"<User id={u.id} user_name={u.user_name} first_name={u.first_name} last_name={u.last_name} email={u.email}>"
 
 class Job(db.Model):
+        
+    __tablename__ = 'jobs'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     job_title = db.Column(db.Text, nullable=False, unique=True)
     location = db.Column(db.Text, nullable=False)
     start_year = db.Column(db.Integer, nullable=False)
     day_rate = db.Column(db.Integer, nullable=False, unique=True)
     user_name = db.Column(db.Text, db.ForeignKey('users.user_name'))
+
+    users = db.relationship('User', backref='jobs')
