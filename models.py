d73e22c1010e2bac506c72bc3ea8e5b4ea96e8bb
megan <msmeganmcmanus@gmail.com>
2020-12-01 22:01:01 -0800

    update initial routes and alaska

--- models.py
+++ models.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
+from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-#from flask_bcrpyt import Bcrypt
 
-
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
@@ -26,6 +28,47 @@ class User(db.Model):
                           nullable=False, default=DEFAULT_IMG)
     password = db.Column(db.Text, nullable=False)
 
+    @classmethod
+    def signup(cls, user_name, first_name, last_name, email, password, image_url):
+        """Sign up user.
+
+        Hashes password and adds user to system.
+        """
+
+        hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
+
+        user = User(
+            user_name=user_name,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=hashed_pwd,
+            image_url=image_url,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, user_name, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If can't find matching user (or if password is wrong), returns False.
+        """
+
+        user = cls.query.filter_by(user_name=user_name).first()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(password, user.password)
+            if is_auth:
+                return user
+
+        return False
+
     def __repr__(self):
         u = self
         return f"<User id={u.id} user_name={u.user_name} first_name={u.first_name} last_name={u.last_name} email={u.email}>"
@@ -42,4 +85,6 @@ class Job(db.Model):
         db.Integer,
         db.ForeignKey('users.id', ondelete='cascade')
     )
+
+
 
