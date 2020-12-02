709e995c125c864abd8ccc3a041165f9244c5f9a
megan <msmeganmcmanus@gmail.com>
2020-12-01 22:01:01 -0800

    update initial routes and alaska

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
 from models import db, connect_db, User, Job
 from forms import NewUserForm, LoginForm
+from sqlalchemy.exc import IntegrityError
 import os
 
 app = Flask(__name__)
+
+CURR_USER_KEY = "curr_user"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL', 'postgresql:///psopayscale2')
@@ -42,6 +45,30 @@ def page_not_found(e):
 
 
 #### USERS ROUTES #####
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Logout user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
 @app.route("/user/new", methods=["GET"])
 def users_new_form():
     """Show a form to create a new user"""
@@ -63,6 +90,32 @@ def add_user():
     return redirect('/home')
     # return redirect('/user/info/<int:user_id>')
 
+@app.route('/user/login', methods=["GET"])
+def login_form():
+    """Show a form to login"""
+
+    form = LoginForm()
+
+    return render_template('login.html', form=form)
+
+@app.route('/user/login', methods=["POST"])
+def login():
+    """Submit user's login info"""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.authenticate(form.user_name.data,
+                                 form.password.data)
+
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.user_name}!", "success")
+            return redirect("/home")  #eventually switch to user's detail's page
+
+        flash("Invalid credentials.", 'danger')
+
+    return render_template('login.html', form=form)
 
 @app.route("/user/info/<int:user_id>", methods=["GET"])
 def user_page(user_id):
@@ -100,11 +153,6 @@ def submit_edit(user_id):
 
 
 
-
-
-
-
-#### AUTHENTICATION #####
 
 
 
@@ -244,3 +292,6 @@ def ocean32_page():
 
 
 #### INDIVIDUAL COUNTRIES ROUTES ####
+@app.route("/alaska", methods=["GET"])
+def alaska():
+    return render_template('/countries/alaska.html')
