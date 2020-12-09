8621b4d1288a6992190c11974ada8f18925d6217
megan <msmeganmcmanus@gmail.com>
2020-12-08 20:15:06 -0800

    update seed

--- seed.py
+++ seed.py
@@ -1,48 +1,6 @@
 from models import db, User, Job
 from app import app
 
-# db.drop_all()
+db.drop_all()
 db.create_all()
 
-
-
-
-
-j1 = Job(
-    job_title="PSO",
-    location="Antarctica",
-    start_year='2019',
-    day_rate='175',
-    cont_company='RPS',
-    user_id='1'
-)
-
-j2 = Job(
-    job_title="PSO",
-    location="Alaska",
-    start_year='2019',
-    day_rate='175',
-    cont_company='RPS',
-    user_id='1'
-)
-
-j3 = Job(
-    job_title="Lead PAM",
-    location="Antarctica",
-    start_year='2019',
-    day_rate='175',
-    cont_company='RPS',
-    user_id='1'
-)
-
-j4 = Job(
-    job_title="Lead PAM",
-    location="Alaska",
-    start_year='2019',
-    day_rate='175',
-    cont_company='RPS',
-    user_id='1'
-)
-
-db.session.add_all([j1, j2, j3, j4])
-db.session.commit()
